0d23672a37d7fb7007d81b521f7739a0ff435811
arundevpkl <arundev.pkl@gmail.com>
2022-08-14 14:15:15 +0530

    project added

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import uiSlice from './reducers/ui-slice';
+import cartSlice from './reducers/cart-slice';
+
+const store = configureStore({
+  reducer: {
+    ui: uiSlice.reducer,
+    cart: cartSlice.reducer
+  }
+});
+
+export default store;
